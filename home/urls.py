34e01b9edecca570c0ad1cdbddb0c7f18bd1129b
ایمان جاهد <101338494+ImanJahed@users.noreply.github.com>
2023-08-19 11:07:49 +0330

    added contact us and about us page
    + Add Contact us form
    + Add Contact Us View
    + Add About Us View

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.Home.as_view(), name='index')
+    path('', views.Home.as_view(), name='index'),
+    path('contact', views.ContacUsView.as_view(), name='contact'),
+    path('about', views.AboutUsView.as_view(), name='about')
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
 class Home(View):
-    
+
     def get(self, request):
         return render(request, 'home/index.html')
+
+
+
+class ContacUsView(View):
+    template_name = 'home/contact.hmtl'
+    form_class = ContactForm
+
+    def get(self,request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your message successfuly sent")
+
+        return render(request, self.template_name, {'form': form})
+
+
+
+class AboutUsView(TemplateView):
+    template_name = 'home/about.html'
