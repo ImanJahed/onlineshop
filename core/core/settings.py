c004d6f5fdfd7a8c6839e097d27e5e964c52598a
Iman Jahed <101338494+ImanJahed@users.noreply.github.com>
2024-04-03 12:51:54 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -182,5 +182,6 @@ if SHOW_DEBUGGER_TOOLBAR:
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
+LOGOUT_REDIRECT_URL = 'website:index'
 
 CELERY_BROKER_URL = 'amqp://localhost'
