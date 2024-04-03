ede71d482f3c33ac01e88044d1d4a29a52f76922
Iman Jahed <101338494+ImanJahed@users.noreply.github.com>
2024-04-03 12:48:09 +0330

    Update urls.py

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -8,15 +8,15 @@ from . import views
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("password-rest/", views.ResetPasswordView.as_view(), name="password_reset"),
+    path("password-reset/", views.ResetPasswordView.as_view(), name="password_reset"),
     path(
         "password-confirm/<uidb64>/<token>",
         auth_views.PasswordResetConfirmView.as_view(
-            template_name="accounts/passwrord_rest_temp/password_rest_confirm.html"
+            template_name="accounts/password_reset_temp/password_reset_confirm.html"
         ),
         name="password_rest_confirm",
     ),
     path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/passwrord_rest_temp/password_rest_complete.html'),
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_temp/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
