42a62b79bbd9a22b2720dce393eda5fcd05c7ba8
Iman Jahed <101338494+ImanJahed@users.noreply.github.com>
2024-04-19 17:19:28 +0330

    Update cart.py

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -5,26 +5,11 @@ class Cart:
 
     def __init__(self, request) -> None:
         self.request = request
-        # cart = self.request.session.get('cart')
+        
         cart = self.request.session.setdefault('cart', {})
-
-        # if not cart:
-        #     cart = self.request.session['cart'] = {}
 
         self.cart = cart
 
-
-    # def add(self, product):
-    #     product_id = product.id
-    #     if product_id not in self.cart:
-    #         self.cart[product_id] = {
-    #             "quantity": 1,
-    #             "price": int(product.price),
-    #         }
-    #     else:
-    #         if self.cart[product_id]["quantity"] < product.inventory:
-    #             self.cart[product_id]["quantity"] += 1
-    #     self.save()
 
     def add_to_cart(self, product_id):
         if product_id in self.cart:
@@ -51,10 +36,10 @@ class Cart:
         product_ids = self.cart.keys()
         products = ProductModel.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
+        
         for product in products:
-
             cart[str(product.pk)]['product'] = product
-            print(cart)
+            
         for item in cart.values():
             item['total_product_price'] = item['product'].price * item['quantity']
             yield item
@@ -65,6 +50,7 @@ class Cart:
     def remove_item(self, product_id):
         del self.cart[product_id]
         self.save()
+        
     def save(self):
         self.request.session.modified = True
 
